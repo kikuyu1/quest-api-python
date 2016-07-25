a49fa92e85fb2b302b8ed2a8d53bf15feff095e3
Edward Muya Mwangi <muyaedward@gmail.com>
2016-07-25 14:39:52 +0300

    Update shedule.py

--- examples/shedule.py
+++ examples/shedule.py
@@ -1,5 +1,5 @@
 from questsms import ScheduleSMS
 
-api_key = '3fc1e09e231e517688410ab8256a8402:EFs6G8wAeriygvaRvalj8XkjR8W87DvC'
-sms = ScheduleSMS(api_key=api_key, sendondate='2017-07-23 4:30:00', sender='Quest-web', username='nderitupatrick4', message='fracis sheduled', to='+254729930509')
+api_key = 'yourapikey'
+sms = ScheduleSMS(api_key=api_key, sendondate='2017-07-23 4:30:00', sender='Quest-web', username='yourusername', message='your scheduled message', to='+25472*****')
 sms.schedule()
