60d10ea95644f18e90271ee78de26310e09859d2
Edward Muya Mwangi <muyaedward@gmail.com>
2016-07-25 14:41:22 +0300

    Update test.py

--- examples/test.py
+++ examples/test.py
@@ -1,5 +1,5 @@
 from questsms import SendSms
-api_key = '3fc1e09e231e517688410ab8256a8402:EFs6G8wAeriygvaRvalj8XkjR8W87DvC'
-sms = SendSms(api_key=api_key, sender='Quest-web', username='nderitupatrick4', message='hi everyone', to=['+254729930509', '+254702681502'])
+api_key = 'yourapikey'
+sms = SendSms(api_key=api_key, sender='Quest-web', username='yourusername', message='your message', to=['+2547******', '+2547******'])
 
 sms.send()
