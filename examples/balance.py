dbb9bb7148036b816d550abd27858eaa7f2b127c
Edward Muya Mwangi <muyaedward@gmail.com>
2016-07-25 14:32:49 +0300

    Update balance.py

--- examples/balance.py
+++ examples/balance.py
@@ -1,5 +1,5 @@
 from questsms import CheckBalance
-api_key = '3fc1e09e231e517688410ab8256a8402:EFs6G8wAeriygvaRvalj8XkjR8W87DvC'
-c = CheckBalance(api_key=api_key, username='nderitupatrick4')
+api_key = 'yourapikey'
+c = CheckBalance(api_key=api_key, username='yourusername')
 
 c.check()
